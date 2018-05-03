40bc15d63d89b1e390b3bdcf12df34a16bd4c9bc
meq1986 <meq1986@163.com>
2018-05-03 14:44:36 +0800

    整理代码目录

--- src/com/maeq/javalearning/ref/CalcRunTime.java
+++ src/com/maeq/javalearning/ref/CalcRunTime.java
@@ -5,7 +5,9 @@ package com.maeq.javalearning.ref;
 
 /**
  * @author maeq
- * 计算程序运行时间的两种方式式
+ * 计算程序运行时间的两种方式
+ * 一种，单位 毫秒
+ * 一种，单位 纳秒
  */
 public class CalcRunTime {
 
@@ -13,12 +15,11 @@ public class CalcRunTime {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		
+	  
 		// 以 ms 为单位
 		long startTime = System.currentTimeMillis();    //获取开始时间
 		
-		for(int i=0;i<=10000;i++)
-		{
+		for(int i=0;i<=10000;i++){
 			System.out.println("hello,world");
 		}
 
@@ -28,12 +29,11 @@ public class CalcRunTime {
 		
 		//第二种是以纳秒为单位计算的。
 
-		 long startTime2=System.nanoTime();   //获取开始时间  
+		long startTime2=System.nanoTime();   //获取开始时间  
 
-		 for(int i=0;i<=10000;i++)
-			{
+		for(int i=0;i<=10000;i++){
 				System.out.println("hello,world");
-			}
+		}
 
 		long endTime2=System.nanoTime(); //获取结束时间  
 
