e17c68eb41d697a5fb41e973f5baf091c255c5fa
meq1986 <meq1986@163.com>
2018-05-03 14:11:23 +0800

    add test/TestDeci.java

---
+++ src/com/maeq/javalearning/test/TestDeci.java
@@ -0,0 +1,34 @@
+/**
+ * Project Name:Java-Learning
+ * File Name:TestDeci.java
+ * Package Name:com.maeq.javalearning.test
+ * Date:2018年4月11日下午1:40:55
+ * Copyright (c) 2018, meq1986@163.com All Rights Reserved.
+ *
+*/
+
+package com.maeq.javalearning.test;
+
+import java.math.BigDecimal;
+
+/**
+ * ClassName:TestDeci <br/>
+ * Function: TODO ADD FUNCTION. <br/>
+ * Reason:	 TODO ADD REASON. <br/>
+ * Date:     2018年4月11日 下午1:40:55 <br/>
+ * @author   Administrator
+ * @version  
+ * @since    JDK 1.6
+ * @see 	 
+ */
+public class TestDeci {
+
+  public static void main(String[] args) {
+
+    // TODO Auto-generated method stub
+    BigDecimal monthRate=new BigDecimal("12");
+    System.out.println(monthRate);
+  }
+
+}
+
