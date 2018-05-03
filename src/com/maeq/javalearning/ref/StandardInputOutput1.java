2b2d79b431497dae89a1e6b7a8a8e99dd995c0df
meq1986 <meq1986@163.com>
2018-05-03 14:23:00 +0800

    test

--- src/com/maeq/javalearning/ref/StandardInputOutput1.java
+++ src/com/maeq/javalearning/ref/StandardInputOutput1.java
@@ -2,6 +2,16 @@ package com.maeq.javalearning.ref;
 
 import java.io.IOException;
 
+/**
+ * ClassName: StandardInputOutput1 <br/>
+ * Function: TODO ADD FUNCTION. <br/>
+ * Reason: TODO ADD REASON(可选). <br/>
+ * date: 2018年5月3日 下午2:22:49 <br/>
+ *
+ * @author Administrator
+ * @version 
+ * @since JDK 1.6
+ */
 public class StandardInputOutput1 {
 
 	public static void main(String[] args) {
